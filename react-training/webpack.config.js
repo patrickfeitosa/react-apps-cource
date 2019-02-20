a412fa174f9ab735469a5f81bf5f6076f13a973b
Patrick Vieira Feitosa <patrick.vieira@enext.local>
2019-02-20 20:18:00 -0300

    Criado primeiro componente React

---
+++ react-training/webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, './src/common/js/index.jsx'),
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: './bundle.js',
+    },
+    devServer: {
+        port: 8080,
+        contentBase: path.join(__dirname, './dist'),
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread'],
+            }
+        }],
+    }
+}
