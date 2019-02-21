52d616ba158a58bbdbf067da6da41e484b94a53d
Patrick Vieira Feitosa <patrick.vieira@enext.local>
2019-02-20 21:58:07 -0300

    Implementado componentes com filhos

--- react-training/src/common/js/index.jsx
+++ react-training/src/common/js/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Component from './components/singleComponent';
 import { First, Second } from './components/multipleComponents';
+
+import SilvaFamily from './components/silvaFamily';
+
+import Family from './components/family';
+import Member from './components/member';
 
 ReactDOM.render(
     <div>
         <Component value='Show!' />
         <First />
         <Second />
+        <SilvaFamily />
+        <Family familyName='Silva'>
+            <Member name='Guilherme' lastName='Silva'/>
+        </Family>
     </div>, document.querySelector('#app')
 );
