58ecd217026ecc5b9e7fb7a26caf6f145c6e57dc
Patrick Vieira Feitosa <patrick.vieira@enext.local>
2019-02-23 10:56:20 -0300

    Implementado Middleware para gerenciar o CORS

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -1,10 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const allowCors = require('./cors.js');
 
 const server = express();
 const port = 3003;
 
 server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(port, () => {
     console.log(`Backend is running on port ${port}`);
