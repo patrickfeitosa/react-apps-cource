52f094af4eab052dbfae5dc61b77b0aeba63e047
Patrick Vieira Feitosa <patrick.vieira@enext.local>
2019-02-24 13:07:38 -0300

    Implementado menu de navegação

--- todo-app/frontend/src/common/js/main/app.jsx
+++ todo-app/frontend/src/common/js/main/app.jsx
@@ -2,11 +2,15 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
+
 import Todo from '../todo/todo.jsx';
 import About from '../about/about.jsx';
 
+import Menu from '../template/menu.jsx';
+
 export default (props) => (
     <div className='container'>
+        <Menu />
         <Todo />
         <About />
     </div>
